109f08a73c3d1ac5a5c28379af488117bcf6daa7
George Lai <georgelai@Georges-MacBook-Pro.local>
2011-10-18 15:29:40 -0700

    Initial Commit

---
+++ Hello Obj C/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Hello Obj C
+//
+//  Created by George Lai on 10/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
